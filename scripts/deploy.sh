261de824f4c653c43dab120ae621711a8c09b34d
Kim-Yong-Kyeong <kyk4957@naver.com>
2023-06-16 04:58:41 +0900

    배포 자동화 구성: deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,42 +4,39 @@
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=practice_springboot_aws
 
-# git clone 받은 위치로 이동
-cd $REPOSITORY/$PROJECT_NAME/
-
-# master 브랜치의 최신 내용 받기
-echo "> Git Pull"
-git pull
-
-# build 수행
-echo "> 프로젝트 Build 시작"
-./gradlew build
-
-echo "> step1 directory로 이동"
-cd $REPOSITORY
-
 # build의 결과물 (jar 파일) 특정 위치로 복사
 echo "> build 파일 복사"
-cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+#cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
+CURRENT_PID=$(pgrep -f $PROJECT_NAME)
+#CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
 
 echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
-        echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-        echo "> kill -15 $CURRENT_PID"
-        kill -15 $CURRENT_PID
-        sleep 5
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+#JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
 
 echo "> Jar Name: $JAR_NAME"
+echo "> $JAR_NAME 에 실행권한 추가"
+chmod +x $JAR_NAME
 
+echo "> $JAR_NAME 실행"
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
-        -Dspring.profiles.active=real \
-        $REPOSITORY/$JAR_NAME 2>&1 &
+        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+        -Dspring.profile.active=real \
+        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
+#nohup java -jar \
+#        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
+#        -Dspring.profiles.active=real \
+#        $REPOSITORY/$JAR_NAME 2>&1 &
